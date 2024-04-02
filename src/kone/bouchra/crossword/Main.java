75bec5524206064a7a36ba6714bd4c96ac1f7a1a
K. Largaton <105814979+Largaton1@users.noreply.github.com>
2024-04-02 21:31:48 +0200

    reamenagement classe principale

--- src/kone/bouchra/crossword/Main.java
+++ src/kone/bouchra/crossword/Main.java
@@ -1,27 +1,29 @@
 package kone.bouchra.crossword;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	public static char[] choice;
+	  public static int choice;
 
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+	public void start(Stage stage) throws Exception {
+        // Charger le fichier FXML
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        Scene scene = new Scene(root);
+        // Définir le titre de la fenêtre
+        stage.setTitle("Jeu de mots croisés");
+        stage.setResizable(false);
+     // Définir la scène sur le stage
+        stage.setScene(scene);
+        // Afficher la fenêtre
+        stage.show();
+    }
 	public static void main(String[] args) {
 		launch(args);
 	}
